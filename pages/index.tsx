ac8cbe864b0f018f883d00a3e56522da27d713c6
ArtExpress <105834340+ArtExpress@users.noreply.github.com>
2022-10-19 16:05:04 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar
      scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
       <Head>
-        <title>Artur's Portfolio</title>
+        <title>Arturs Portfolio</title>
       </Head>
 
       <Header />
